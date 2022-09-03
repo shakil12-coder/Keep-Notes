77018aaf1190787bf053a4d4e864b782eebc1e0a
Shakil Ahmed <shakilahmed0888@gmail.com>
2022-09-03 17:11:21 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+      <div className="center_div">
+        <h1>ToDo List</h1>
+      </div>
     </div>
   );
 }
